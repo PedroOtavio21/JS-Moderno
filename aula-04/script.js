1c8409d6fdea2ad168068f527a8339fd82d521ac
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-08-27 21:20:44 -0300

    Adiciona aula 04 de JS IV, contendo explicações sobre a funcionalidade 'Template Literals'

---
+++ aula-04/script.js
@@ -0,0 +1,10 @@
+/**Template Literals
+ * Nova forma de um conteúdo de setrings
+ * Permite concatenação de setrings, inserção de valores e quebra de linha automática
+ * `` <=> '' || ""
+ */
+
+const texto = 'Mundo'
+console.log(`Olá, ${texto}!
+Estamos em ${Date()}
+    2 + 2 = ${2 + 2}`)
