6c9fa54c65faac63f6f3ef9a0aec3968f8b6b18b
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-08-27 21:45:17 -0300

    Adiciona exemplo de desestruturação com arrays e funções

--- aula-06/script.js
+++ aula-06/script.js
@@ -6,6 +6,7 @@
  * -    Ex: const {name, email} = person
  * Arrays:
  * -    pode-se atribuir qualquer nome para a variável
+ * -    a ordem é de EXTREMA importancia!
  * -    const [x,y] = coordinates
  * 
  * Ex: 
@@ -14,6 +15,7 @@
  *  }
  */
 
+// Objetos
 const person = {
     name: 'Luke',
     job: 'Farmer',
@@ -24,3 +26,23 @@ const name = person.name
 const {job, parents} = person
 
 console.log(name, job, person)
+
+// Arrays
+const [father, mother] = parents
+
+console.log(father, mother)
+
+// Functions
+function createUser({name, job, parents}){
+    const id = Math.floor(Math.random() * 9999)
+    return {
+        id, 
+        name,
+        job,
+        parents
+    }
+}
+
+const luke = createUser(person)
+
+console.log(luke)
