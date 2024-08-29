4de5a4c0f8e7f17e76b6ce1d410c679b492fe8e1
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-08-29 15:00:43 -0300

    Adiciona manutenção ao adquirir botão de resultado para cópia em copyToClipboard.js

--- ex-02/js/copyToClipboard.js
+++ ex-02/js/copyToClipboard.js
@@ -1,9 +1,10 @@
 export default function copyToClipboard(ev) {
+    const resultInput = document.getElementById("result");
     const button = ev.currentTarget;
     if (button.innerText === "Copy") {
       button.innerText = "Copied!";
       button.classList.add("success");
-      navigator.clipboard.writeText(document.querySelector("#result").value);
+      navigator.clipboard.writeText(resultInput.value);
     } else {
       button.innerText = "Copy";
       button.classList.remove("success");
