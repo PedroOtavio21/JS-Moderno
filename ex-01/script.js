c95061b4853f22363ab22431a524680a09d142f5
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-08-29 10:20:19 -0300

    Adiciona segunda parte do exercício 01 em JS IV

--- ex-01/script.js
+++ ex-01/script.js
@@ -43,6 +43,27 @@ console.log(`Média Ponderada: ${mediaPonderada(
     {numero: 10, peso: 1}
 )}`)
 
-// const mediana = (...numeros) => {
-    
-// }
+const mediana = (...numeros) => {
+    const numerosOrdenados = [...numeros].sort((a, b) => a - b)
+    const meio = Math.floor(numerosOrdenados.length / 2)
+    if(numerosOrdenados.length % 2 !== 0){
+        return numerosOrdenados[meio]
+    }
+    const primeiroMeio = numerosOrdenados[meio - 1]
+    const segundoMeio = numerosOrdenados[meio]
+    return mediaSimples(primeiroMeio, segundoMeio)
+}
+
+console.log(`Mediana com 1 número: ${mediana(1,3,2,7,5)}`)
+console.log(`Mediana com 2 números: ${mediana(1,3,2,5,7,4)}`)
+
+const moda = (...numeros) => {
+    const quantidade = numeros.map(numero => [
+        numero,
+        numeros.filter(n => numero === n).length
+    ])
+    quantidade.sort((a,b) => b[1] - a[1])
+    return quantidade[0][0]
+}
+
+console.log(`Moda: ${1,1,3,4,2,4,5,7,6,4,1,2,4}`)
